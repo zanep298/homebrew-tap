0065c5974c300eae2b3f93dbb67895a6b4ee852a
zanep298 <minh.tan2908@gmail.com>
2026-05-11 04:29:18 +0000

    zf v0.0.2

--- Formula/zf.rb
+++ Formula/zf.rb
@@ -7,16 +7,16 @@ class Zf < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/zanep298/zforge/releases/download/v#{version}/zf-aarch64-apple-darwin.tar.gz"
-      sha256 "0ba4fc52393e55d1fe40dddf37f0b2f734147e1eabcc2cd9f22e3a7c8995f4f0"
+      sha256 "a883fb31e4e18ab9fa511d2e565b2585a56e4ee5552461a2ea178d5d7b3a0ecb"
     else
       url "https://github.com/zanep298/zforge/releases/download/v#{version}/zf-x86_64-apple-darwin.tar.gz"
-      sha256 "196c34efcc32e1fc63adeec405bdf83d9decc7f57f80422c70de30f2f44bdb0d"
+      sha256 "f6b13e541973f7c53c31a55170c0c2d07514a9bf08b1bfc98b6353febeb43c73"
     end
   end
 
   on_linux do
     url "https://github.com/zanep298/zforge/releases/download/v#{version}/zf-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "6526f3ee4f1ad092b46a5dd633236b0cb886f7d2917a0a8029e586ce9fa7a2f0"
+    sha256 "4311a531fd3400046b90b8ab80bd05372113ee6bdb27217d0dd31f1c8d09a353"
   end
 
   def install
