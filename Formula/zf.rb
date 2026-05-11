dea281e37d5095adb88b35bb9a8958b394baa931
zanep298 <minh.tan2908@gmail.com>
2026-05-11 11:41:07 +0700

    feat: rename binary to zforge, update URLs to zforge-* artifacts

--- Formula/zf.rb
+++ Formula/zf.rb
@@ -6,24 +6,24 @@ class Zf < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/zanep298/zforge/releases/download/v#{version}/zf-aarch64-apple-darwin.tar.gz"
-      sha256 "d1fd664b504a0921e5e5f8b3f211b587e6bb421d0a0876c88ad806194e768244"
+      url "https://github.com/zanep298/zforge/releases/download/v#{version}/zforge-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_ARM64"
     else
-      url "https://github.com/zanep298/zforge/releases/download/v#{version}/zf-x86_64-apple-darwin.tar.gz"
-      sha256 "06dfb22a112d280f83071c7cc7e0320036a481173d251594592a35e1daaafb25"
+      url "https://github.com/zanep298/zforge/releases/download/v#{version}/zforge-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_X86_64_DARWIN"
     end
   end
 
   on_linux do
-    url "https://github.com/zanep298/zforge/releases/download/v#{version}/zf-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "620b756bd79b337c513b31553a26cfb371c608651fb2278e8482e4c615558843"
+    url "https://github.com/zanep298/zforge/releases/download/v#{version}/zforge-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "PLACEHOLDER_SHA256_LINUX"
   end
 
   def install
-    bin.install "zf"
+    bin.install "zforge"
   end
 
   test do
-    system "#{bin}/zf", "--version"
+    system "#{bin}/zforge", "--version"
   end
 end
