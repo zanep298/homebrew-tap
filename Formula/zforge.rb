7aa98a4f7958275c44a1ef9377d152b38af8861f
zanep298 <minh.tan2908@gmail.com>
2026-05-12 13:21:31 +0000

    zforge v0.0.13

--- Formula/zforge.rb
+++ Formula/zforge.rb
@@ -1,22 +1,22 @@
 class Zforge < Formula
   desc "TDD-first AI development workflow CLI"
   homepage "https://github.com/zanep298/zforge"
-  version "0.0.12"
+  version "0.0.13"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/zanep298/zforge/releases/download/v#{version}/zforge-aarch64-apple-darwin.tar.gz"
-      sha256 "b85f3803511bf7fb7c09be9a7d63e1a5c0b6b33a0767e9b7a592155a220e4c0c"
+      sha256 "1086356c426185b55980424dbc901df23e553dc2f3e05f38fef493d80aaaec02"
     else
       url "https://github.com/zanep298/zforge/releases/download/v#{version}/zforge-x86_64-apple-darwin.tar.gz"
-      sha256 "0562e4e60941753d9e08c7f1e47a024df09c9e6bca53608859bb5a5e01b78c96"
+      sha256 "3d6e3a73ab99ed175c1d1e77744a5517c53b811c69417a3cd24dfa3f25923531"
     end
   end
 
   on_linux do
     url "https://github.com/zanep298/zforge/releases/download/v#{version}/zforge-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "caad597cc20e4eb753f672eb2f4589d1cf62de2e10a6276016c929f296870d59"
+    sha256 "091d738a7d3d25c9e6813cb6c70a57a7043771f43f2a449d4a3912ba284215b4"
   end
 
   def install
